f60de920341b3ba455927990b1db7dd018196de8
Iman Lzr <imanelaazraoui@gmail.com>
2024-11-27 15:19:12 +0100

    final design

---
+++ app/src/main/java/com/example/alarm/Splash.kt
@@ -0,0 +1,19 @@
+package com.example.alarm
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
